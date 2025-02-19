5318d5c4dbafde6322df877e30551b84ececb18e
Kevin Ma <akevingma@gmail.com>
2025-02-19 15:59:25 -0600

    working again

--- apps/native/vite.config.ts
+++ apps/native/vite.config.ts
@@ -8,6 +8,11 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [react()],
 
+  define: {
+    "process.env": {},
+    "process": {},
+  },
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
